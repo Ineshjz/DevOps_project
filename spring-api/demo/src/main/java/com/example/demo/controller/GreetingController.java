428fef0458fcfbba56f92b182c64d9fd903d0e53
ines <ines.hijazi@epfedu.fr>
2026-06-03 16:17:27 +0200

    test Hello Ines

--- spring-api/demo/src/main/java/com/example/demo/controller/GreetingController.java
+++ spring-api/demo/src/main/java/com/example/demo/controller/GreetingController.java
@@ -10,7 +10,7 @@ public class GreetingController {
    private final AtomicLong counter = new AtomicLong();
 
    @GetMapping("/")
-   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+   public Greeting greeting(@RequestParam(value = "name", defaultValue = "INES") String name) {
        return new Greeting(counter.incrementAndGet(), String.format(template, name));
    }
 
