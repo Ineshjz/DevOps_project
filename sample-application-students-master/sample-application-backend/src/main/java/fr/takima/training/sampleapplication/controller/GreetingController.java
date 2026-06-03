97ee3ca924a037decbdea34d71127007c47ce750
ines <ines.hijazi@epfedu.fr>
2026-06-03 16:38:17 +0200

    Test2 hello ines

---
+++ sample-application-students-master/sample-application-backend/src/main/java/fr/takima/training/sampleapplication/controller/GreetingController.java
@@ -0,0 +1,13 @@
+package fr.takima.training.sampleapplication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Ines";
+    }
+}
